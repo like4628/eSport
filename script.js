53ba5cd1b51cdc9bbf3a089ec6515b45c82d690b
sujunjia <llyamk56@gmail.com>
2019-08-16 17:20:19 +0800

    add bubble

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-$(window).scroll(function(evt){
-  if($(window).scrollTop()>0)
-    {
-      $('.navbar').removeClass('navbar-top')
-    }else{
-      $('.navbar').addClass('navbar-top')
-    }
+$(window).scroll(function(evt) {
+  if ($(window).scrollTop() > 0) {
+    $('.navbar').removeClass('navbar-top')
+  } else {
+    $('.navbar').addClass('navbar-top')
+  }
 });
 
 var s = skrollr.init();
+
+//泡泡特效
+function bubbles() {
+  $.each($(".particletext.bubbles"), function() {
+    var bubblecount = ($(this).width() / 50) * 10;
+    for (var i = 0; i <= bubblecount; i++) {
+      var size = ($.rnd(40, 80) / 10);
+      //这里的animation-delay很重要，关系到你的特效是否看上去是连续无断层的，上一步中css是18秒，所以这一步中延迟时间就设置成$.rnd(0,180)/10)
+      $(this).append('<span class="particle" style="top:' + $.rnd(20, 80) + '%; left:' + $.rnd(0, 95) + '%;width:' + size + 'px; height:' + size + 'px;animation-delay: ' + ($.rnd(0, 180) / 10) + 's;"></span>');
+    }
+  });
+}
+jQuery.rnd = function(m, n) {
+  m = parseInt(m);
+  n = parseInt(n);
+  return Math.floor(Math.random() * (n - m + 1)) + m;
+}
+bubbles();
